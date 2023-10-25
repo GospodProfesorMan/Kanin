d5983c3f16b5d6b9a6d9699c349c1b573031697b
GospodProfesorMan <fmfiroylight@gmail.com>
2023-10-25 18:19:09 +0200

    remove Unnecesarry dependencies, resolved conflict

--- frontend/src/component/screens/templates/test.js
+++ frontend/src/component/screens/templates/test.js
@@ -1,7 +1,4 @@
-import ReactHtmlParser from 'react-html-parser';
 export default function Raw(input) {
     //console.log("displaying raw input: " + input);
-    return (<div>
-        {ReactHtmlParser(input)}
-    </div>)
+    return (<div dangerouslySetInnerHTML={{__html: input}}></div>)
 }
